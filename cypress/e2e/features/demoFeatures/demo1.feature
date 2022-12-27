7826604d5d5be1e86c0b0875cc7265d9c6273c51
Deepak Sarangi <dpksarangi@gmail.com>
2022-12-27 22:53:34 +0530

    first commit

---
+++ cypress/e2e/features/demoFeatures/demo1.feature
@@ -0,0 +1,7 @@
+Feature: Demo feature one
+    Scenario: Scenario name
+        Given I open the base page
+        Then I click on top menu
+        When I verify title
+        Then Title matches
+
---
+++ cypress/support/step_definitions/Demo1.js
@@ -0,0 +1,15 @@
+import { Given, Then, When } from "@badeball/cypress-cucumber-preprocessor";
+
+Given("I open the base page", function () {
+  cy.visit("https://practice.automationtesting.in/");
+});
+Then("I click on top menu", function () {
+  cy.get("#menu-icon").click();
+  cy.get("#menu-item-40").click();
+});
+When("I verify title", function () {
+  cy.title().should("include", "Products");
+});
+Then("Title matches", function () {
+  console.log("Everything matches");
+});
